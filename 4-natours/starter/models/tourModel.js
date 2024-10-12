e51b889b650755cf131a6772745de40d2a2499dd
sina khaksar <sinakhaksar3@gmail.com>
2024-10-13 00:02:57 +0330

    ep27 Data Validation Built-In Validators is Done

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -8,6 +8,14 @@ const tourSchema = new mongoose.Schema(
 			required: [true, "A Tour MUST have a name"],
 			unique: true,
 			trim: true,
+			maxLength: [
+				40,
+				"A tour name must be shorter or equal to 40 characters",
+			],
+			minLength: [
+				10,
+				"A tour name must be longer or equal to 10 characters",
+			],
 		},
 		slug: String,
 		duration: {
@@ -21,8 +29,17 @@ const tourSchema = new mongoose.Schema(
 		difficulty: {
 			type: String,
 			required: [true, "A Tour MUST have a  difficulty level"],
+			enum: {
+				values: ["easy", "medium", "difficult"],
+				message: "Difficulty is either easy, medium or difficult",
+			},
 		},
-		ratingsAverage: { type: Number, default: 4.5 },
+		ratingsAverage: {
+			type: Number,
+			default: 4.5,
+			min: [1, "Rating must be above 1.0"],
+			max: [5, "Rating must be below 5.0"],
+		},
 		ratingsQuantity: { type: Number, default: 0 },
 		price: { type: Number, required: [true, "A Tour MUST have a price"] },
 		priceDiscout: Number,
