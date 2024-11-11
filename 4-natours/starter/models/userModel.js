8f56fdbf356f7728c45d7e8c453fe1f422f89658
sina khaksar <sinakhaksar3@gmail.com>
2024-11-11 09:13:20 +0330

    before auth section

---
+++ 4-natours/starter/models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, "Please provide your name!"],
+	},
+	email: {
+		type: String,
+		required: [true, "Please provide your eamil!"],
+		unique: true,
+		lowercase: true, // converts the input to lowercase
+		validate: [validator.isEmail, "Please provide a valid email!"],
+	},
+	photo: String,
+	password: {
+		type: String,
+		required: [true, "Please provide a password"],
+		minlength: 8,
+	},
+	passwordConfirm: {
+		type: String,
+		required: [true, "Please confirm your password "],
+		validate: {
+			validator: function (el) {
+				return el === this.password;
+			},
+			message: "Passwords are not the same!",
+		},
+	},
+});
+
+userSchema.pre("save", async function (next) {
+	//if password is !modified next
+	if (!this.isModified("password")) return next();
+	//hash the password with cost of 12.
+	// 13 to 14 in cost would double the time of response.
+	this.password = await bcrypt.hash(this.password, 13);
+	// Delete the passwordConfirm. we only needed it for
+	this.passwordConfirm = undefined;
+	next();
+});
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
