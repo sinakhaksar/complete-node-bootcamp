eece44db8a217c02d47f95986487f4caed212b48
sina khaksar <sinakhaksar3@gmail.com>
2024-11-29 20:42:32 +0330

    imported data... users, reviews annd tours are now more realistic

--- 4-natours/starter/dev-data/data/import-dev-data.js
+++ 4-natours/starter/dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const Tour = require("../../models/tourModel");
+const User = require("../../models/userModel");
+const Review = require("../../models/reviewModel");
 
 dotenv.config({ path: "../../.env" });
 
@@ -21,19 +23,27 @@ mongoose
 	});
 
 // read json file
-let tours;
-try {
-	// => javaScript object
-	tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8")); //JSON
-} catch (err) {
-	console.error("💥Error reading json file!");
-	process.exit(1);
-}
+// let tours;
+// try {
+// 	// => javaScript object
+// 	tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8")); //JSON
+// } catch (err) {
+// 	console.error("💥Error reading json file!");
+// 	process.exit(1);
+// }
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
+const reviews = JSON.parse(
+	fs.readFileSync(`${__dirname}/reviews.json`, "utf-8"),
+);
 //import to db
 
 const importData = async () => {
 	try {
 		await Tour.create(tours);
+		await User.create(users, { validateBeforeSave: false });
+		await Review.create(reviews);
+
 		console.log("Data Successfully loaded! ");
 		process.exit(0);
 	} catch (err) {
@@ -46,6 +56,8 @@ const importData = async () => {
 const deleteData = async () => {
 	try {
 		await Tour.deleteMany();
+		await Review.deleteMany();
+		await User.deleteMany();
 		process.exit(0);
 	} catch (err) {
 		console.error(`💥${err}`);
@@ -61,4 +73,5 @@ if (process.argv[2] === "--import") {
 	console.log("all data deleted");
 } else {
 	console.error("Invalid command!");
+	process.exit(1);
 }
